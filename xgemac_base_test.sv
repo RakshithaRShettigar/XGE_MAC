2cd3a4cd3cb8e79565d4d1c34d0e69b631331e56
dhanasekarp <126143349+dhanasekarp03@users.noreply.github.com>
2024-07-01 16:35:24 +0530

    Create xgemac_base_test.sv

---
+++ xgemac_base_test.sv
@@ -0,0 +1,27 @@
+class xgemac_base_test extends uvm_test;
+  
+  xgemac_env env_h;
+ 
+  `uvm_component_utils(xgemac_base_test)
+  
+  function new(string name = "xgemac_base_test", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+  
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase); 
+    env_h = xgemac_env::type_id::create("env_h", this);
+  endfunction
+ 
+  task run_phase(uvm_phase phase);
+    
+    super.run_phase(phase); 
+    
+    phase.raise_objection(this);
+   `uvm_info(get_type_name(), $sformatf("Inside BASE_TEST"), UVM_NONE);
+    `uvm_info(get_type_name(), $sformatf("Done BASE_TEST"), UVM_NONE);
+    phase.drop_objection(this);
+    
+  endtask
+  
+endclass
