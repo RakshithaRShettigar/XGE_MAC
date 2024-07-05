c0be5b90e83adb21b2e3b304f03bdf70b5d64cce
Omar Farhan Khalifa <126596915+Omar-farhan1084@users.noreply.github.com>
2024-07-05 14:45:26 +0530

    Update xgemac_normal_vseq.sv

--- src/tb/xgemac_normal_vseq.sv
+++ src/tb/xgemac_normal_vseq.sv
@@ -1,11 +1,20 @@
 `ifndef XGEMAC_NORMAL_VSEQ_INCLUDED
 `define XGEMAC_NORMAL_VSEQ_INCLUDED
 
-
+//--------------------------------------------------------------------------------------------
+// Class: xgemac_normal_vseq
+//--------------------------------------------------------------------------------------------
 class xgemac_normal_vseq extends xgemac_base_vseq;
   `uvm_object_utils(xgemac_normal_vseq)
-
+  
+  //Variable : in_seq_nornal_h
+  //Declaring handle to the virtual sequence
+ 
   in_seq_normal in_seq_normal_h;
+
+  //-------------------------------------------------------
+  // Externally defined Tasks and Functions
+  //-------------------------------------------------------
   
   extern function xgemac_normal_vseq::new(string name = "xgemac_normal_vseq");
   extern task body();
@@ -13,11 +22,20 @@ class xgemac_normal_vseq extends xgemac_base_vseq;
 
 endclass  
 
+//--------------------------------------------------------------------------------------------
+// Construct: new
+//  name : xgemac_normal_vseq
+//--------------------------------------------------------------------------------------------
 
 function xgemac_normal_vseq::new(string name = "xgemac_normal_vseq");
   super.new(name);
 endfunction : new
 
+//--------------------------------------------------------------------------------------------
+// Task : body
+// Used to creates the handle and starts the sequences
+//  name : xgemac_normal_vseq
+//--------------------------------------------------------------------------------------------
    task body();
     `uvm_info(get_type_name(), "virtual_seq: Inside Body", UVM_LOW);
      in_seq_normal_h = in_seq_normal::type_id::create("in_seq_normal_h");
