98b3b571baa6fef8ba668f7dfae451e265eaead5
Rakshitha R <109401667+RakshithaRShettigar@users.noreply.github.com>
2024-07-05 12:07:44 +0530

    Update xgemac_base_vseq.sv

--- src/tb/xgemac_base_vseq.sv
+++ src/tb/xgemac_base_vseq.sv
@@ -12,10 +12,44 @@ class xgemac_base_vseq extends uvm_sequence;
   //Variable : in_seqr_h
   //Declaring handle to the in sequencer
   in_seqr in_seqr_h;
+
+  //Variable : wish_seqr_h
+  //Declaring handle to the wish sequencer
+  wish_seqr wish_seqr_h;
+
+  //Variable : reset_seqr_h
+  //Declaring handle to the reset sequencer
+  reset_seqr reset_seqr_h;
+
+  //Variable : wish_seq_h
+  //Declaring handle to the wish sequence
+  wish_seq wish_seq_h;
+
+  //Variable : reset_seq_h
+  //Declaring handle to the wish sequence
+  reset_seq wish_seq_h;
   
   //-------------------------------------------------------
   // Externally defined Tasks and Functions
   //-------------------------------------------------------
   extern function new(string name = "apb_virtual_base_seq");
   extern task body();
-endclass : apb_virtual_base_seq
+endclass : xgemac_base_vseq
+    
+function xgemac_base_vseq::new(string name = "xgemac_base_vseq");
+  super.new(name);
+endfunction : new
+
+//--------------------------------------------------------------------------------------------
+// Task : body
+// Used to create the wish seqr to master seqr
+//
+// Parameters:
+//  name - apb_virtual_base_seq
+//--------------------------------------------------------------------------------------------
+task xgemac_base_vseq::body();
+  super.body();
+  `uvm_info(get_type_name(), "virtual_seq: Inside Body", UVM_LOW);
+  wish_seq_h = wish_seq::type_id::create("wish_seq_h");
+  reset_seq_h = reset_seq::type_id::create("reset_seq_h");
+endtask
