c5022a0d2f5cf6d3b9bff9000c18a26a60d0d661
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2024-07-05 12:00:59 +0530

    Update in_seqr.sv

--- src/tb/in_seqr.sv
+++ src/tb/in_seqr.sv
@@ -1,15 +1,18 @@
-`ifndef IN_SEQUNCER_INCLUDED
-`define IN_SEQUENCER_INCLUDED
-
+`ifndef IN_SEQUNCER_INCLUDED_
+`define IN_SEQUENCER_INCLUDED_
+//--------------------------------------------------------------------------------------------
+// Class: in_seqr
+//--------------------------------------------------------------------------------------------
 class in_seqr extends uvm_sequencer #(in_seq_item);
+  
+  //factory registration
   `uvm_component_utils(in_seqr)
   
 extern function new(string name = "in_seqr", uvm_component parent = null);
 endclass
   
 //class constructor  
-function in_seqr::new(string name = "in_seqr",
-                                uvm_component parent = null);
+function in_seqr::new(string name = "in_seqr", uvm_component parent = null);
    super.new(name, parent);
 endfunction
 
