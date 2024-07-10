67ebe93c1b6b6ccd4d01756f6e0f18e47992de6d
01sharanshetty <126483491+01sharanshetty@users.noreply.github.com>
2024-07-10 12:29:26 +0530

    Update in_driver.sv

--- src/tb/in_driver.sv
+++ src/tb/in_driver.sv
@@ -30,34 +30,46 @@ virtual task run_phase(uvm_phase phase);
   
     forever begin
       seq_item_port.get_next_item(isi);
-      if(!pkt_vif.pkt_tx_full)
-        begin
       @(posedge pkt_vif.pkt_in_dr_cb)
       begin
-        if(isi.frame.size()>1)
-          begin
-          pkt_vif.pkt_tx_val<=1; //Enabling transaction
-          pkt_vif.pkt_tx_eop<=0;
-          if(pkt_count>0)
-              pkt_vif.pkt_tx_sop<=1;
-          else
-              pkt_vif.pkt_tx_sop<=0;
-          pkt_vif.pkt_tx_data <= isi.frame.pop_back();
-          pkt_count<=pkt_count+1;
-
-          end
-        else 
-          begin
-            pkt_vif.pkt_tx_val<=1; //Enabling transaction
-            pkt_vif.pkt_tx_eop<=1; 
-            pkt_vip.pkt_tx_data <= isi.frame.pop_back();
-            pkt_vip.pkt_tx_mod <= isi.pkt_tx_mod;
-          end
-      end
-        end
+            if(!pkt_vif.pkt_tx_full)
+                begin
+                    if(isi.frame.size()>1)
+                        begin
+                            pkt_vif.pkt_tx_val<=1; //Enabling transaction
+                            pkt_vif.pkt_tx_eop<=0;
+                            if(pkt_count==0)
+                                pkt_vif.pkt_tx_sop<=1;
+                            else
+                                pkt_vif.pkt_tx_sop<=0;
+                            pkt_vif.pkt_tx_data <= isi.frame.pop_back();
+                            pkt_count<=pkt_count+1;
+                          end
+                    else if (isi.frame.size()==1)
+                        begin
+                            pkt_vif.pkt_tx_val<=1; //Enabling transaction
+                            pkt_vif.pkt_tx_eop<=1;
+                            pkt_vif.pkt_tx_sop<=0;
+                            pkt_vip.pkt_tx_data <= isi.frame.pop_back();
+                            pkt_vip.pkt_tx_mod <= isi.pkt_tx_mod;
+                        end
+                    else
+                        begin
+                            pkt_vif.pkt_tx_val<=0; //Enabling transaction
+                            pkt_vif.pkt_tx_eop<=0;
+                            pkt_vif.pkt_tx_sop<=0;
+                        end
+                 end
+              else
+                        begin
+                            pkt_vif.pkt_tx_val<=0;
+                            pkt_vif.pkt_tx_eop<=0;
+                            pkt_vif.pkt_tx_sop<=0;
+                        end
+         end
       
-       seq_item_port.item_done();
-    end
+         seq_item_port.item_done();
+        end
    
 endtask
    
