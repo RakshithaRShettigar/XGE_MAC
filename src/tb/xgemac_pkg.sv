d2986680370d23fe03492f450e95e699bf7dffbd
Rakshitha R <109401667+RakshithaRShettigar@users.noreply.github.com>
2024-07-11 10:58:35 +0530

    Update xgemac_pkg.sv

--- src/tb/xgemac_pkg.sv
+++ src/tb/xgemac_pkg.sv
@@ -1,13 +1,37 @@
-ifndef AXI_ENV_PKG_INCLUDED_
+`ifndef AXI_ENV_PKG_INCLUDED_
 `define AXI_ENV_PKG_INCLUDED_
 package xgemac_pkg;
   `include "uvm_macros.svh"
   import uvm_pkg::*;
-  //design
 
-  
-
-  
-
-
+  `include "in_seq_item.sv"
+  `include "in_seq_normal.sv"
+  `include "in_seq_oversized.sv"
+  `include "in_seq_undersized.sv"
+  `include "reset_seq.sv"
+  `include "wish_seq.sv"
+  `include "wish_seqr.sv"
+  `include "reset_seqr.sv"
+  `include "in_seqr.sv"
+  `include "in_drv.sv"
+  `include "reset_drv.sv"
+  `include "wish_drv.sv"
+  `include "in_mon.sv"
+  `include "out_mon.sv"
+  `include "wish_mon.sv"
+  `include "xgemac_scoreboard.sv"
+  `include "xgemac_base_vseq.sv"
+  `include "xgemac_normal_vseq.sv"
+  `include "xgemac_oversize_vseq.sv"
+  `include "xgemac_undersize_vseq.sv"
+  `include "in_agent.sv"
+  `include "out_agent.sv"
+  `include "wish_agent.sv"
+  `include "reset_agent.sv"
+  `include "xgemac_env.sv"
+  `include "xgemac_base_test.sv"
+  `include "xgemac_normal_test.sv"
+  `include "xgemac_oversize_test.sv"
+  `include "xgemac_undersize_test.sv"
 endpackage
+`endif
