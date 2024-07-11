e6d375b218684962b249b27837518460885c3b55
Rakshitha R <109401667+RakshithaRShettigar@users.noreply.github.com>
2024-07-11 12:18:33 +0530

    Update design_pkg.sv

--- src/tb/design_pkg.sv
+++ src/tb/design_pkg.sv
@@ -4,51 +4,28 @@
 package design_pkg;
 
 
-vlog -timescale 1ps/1ps +incdir+../../rtl/include ../../RTL/verilog/fault_sm.v
+`include "../RTL/verilog/fault_sm.v"
+`include "../RTL/verilog/generic_mem_small.v"
+`include "../RTL/verilog/generic_mem_medium.v"
+`include "../RTL/verilog/generic_fifo_ctrl.v"
+`include "../RTL/verilog/generic_fifo.v"
+`include "../RTL/verilog/meta_sync.v"
+`include "../RTL/verilog/meta_sync_single.v"
+`include "../RTL/verilog/rx_hold_fifo.v"
+`include "../RTL/verilog/rx_data_fifo.v"
+`include "../RTL/verilog/rx_dequeue.v"
+`include "../RTL/verilog/rx_enqueue.v"
+`include "../RTL/verilog/sync_clk_core.v"
+`include "../RTL/verilog/sync_clk_wb.v"
+`include "../RTL/verilog/sync_clk_xgmii_tx.v"
+`include "../RTL/verilog/tx_hold_fifo.v"
+`include "../RTL/verilog/tx_data_fifo.v"
+`include "../RTL/verilog/tx_dequeue.v"
+`include "../RTL/verilog/tx_enqueue.v"
+`include "../RTL/verilog/wishbone_if.v"
+`include "../RTL/verilog/xge_mac.v"
 
-vlog -timescale 1ps/1ps +incdir+../../rtl/include ../../rtl/verilog/generic_mem_small.v
-
-vlog -timescale 1ps/1ps +incdir+../../rtl/include ../../rtl/verilog/generic_mem_medium.v
-
-vlog -timescale 1ps/1ps +incdir+../../rtl/include ../../rtl/verilog/generic_fifo_ctrl.v
-
-vlog -timescale 1ps/1ps +incdir+../../rtl/include ../../rtl/verilog/generic_fifo.v
-
-vlog -timescale 1ps/1ps +incdir+../../rtl/include ../../rtl/verilog/meta_sync.v
-
-vlog -timescale 1ps/1ps +incdir+../../rtl/include ../../rtl/verilog/meta_sync_single.v
-
-vlog -timescale 1ps/1ps +incdir+../../rtl/include ../../rtl/verilog/rx_hold_fifo.v
-
-vlog -timescale 1ps/1ps +incdir+../../rtl/include ../../rtl/verilog/rx_data_fifo.v
-
-vlog -timescale 1ps/1ps +incdir+../../rtl/include ../../rtl/verilog/rx_dequeue.v
-
-vlog -timescale 1ps/1ps +incdir+../../rtl/include ../../rtl/verilog/rx_enqueue.v
-
-vlog -timescale 1ps/1ps +incdir+../../rtl/include ../../rtl/verilog/sync_clk_core.v
-
-vlog -timescale 1ps/1ps +incdir+../../rtl/include ../../rtl/verilog/sync_clk_wb.v
-
-vlog -timescale 1ps/1ps +incdir+../../rtl/include ../../rtl/verilog/sync_clk_xgmii_tx.v
-
-vlog -timescale 1ps/1ps +incdir+../../rtl/include ../../rtl/verilog/tx_hold_fifo.v
-
-vlog -timescale 1ps/1ps +incdir+../../rtl/include ../../rtl/verilog/tx_data_fifo.v
-
-vlog -timescale 1ps/1ps +incdir+../../rtl/include ../../rtl/verilog/tx_dequeue.v
-
-vlog -timescale 1ps/1ps +incdir+../../rtl/include ../../rtl/verilog/tx_enqueue.v
-
-vlog -timescale 1ps/1ps +incdir+../../rtl/include ../../rtl/verilog/wishbone_if.v
-
-vlog -timescale 1ps/1ps +incdir+../../rtl/include ../../rtl/verilog/xge_mac.v
-
-
-
-vlog -timescale 1ps/1ps +incdir+../../rtl/include ../../tbench/verilog/tb_xge_mac.v
-
-
+  
 endpackage
 
 `endif
