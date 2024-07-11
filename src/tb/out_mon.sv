55a9ddb46f5538d71596dcb4f877027e619e2497
Myron2812 <67193041+Myron2812@users.noreply.github.com>
2024-07-11 16:48:34 +0530

    out mon class and pport name change

--- src/tb/out_mon.sv
+++ src/tb/out_mon.sv
@@ -2,11 +2,11 @@
 `define OUT_MONITOR_INCLUDED_
 
 //--------------------------------------------------------------------------------------------
-// Class: out_monitor
+// Class: out_mon
 //  It gets the output data from the DUT and converts them into transaction items and calls the write function
 //--------------------------------------------------------------------------------------------
-class out_monitor extends uvm_monitor; 
-  `uvm_component_utils(out_monitor)
+class out_mon extends uvm_monitor; 
+  `uvm_component_utils(out_mon)
   
   // Variable: pkt_vif
   // Declaring handle for pkt interface
@@ -16,31 +16,31 @@ class out_monitor extends uvm_monitor;
   // Declaring handle for in_seq_item class 
   in_seq_item in_seq_item_h;
     
-  // Variable: out_monitor_port
+  // Variable: out_mon_port
   // Declaring analysis port for the monitor port
-  uvm_analysis_port#(in_seq_item) out_monitor_port;
+  uvm_analysis_port#(in_seq_item) out_mon_port;
   
   //-------------------------------------------------------
   // Externally defined Tasks and Functions
   //-------------------------------------------------------
-  extern function new(string name = "out_monitor", uvm_component parent);
+  extern function new(string name = "out_mon", uvm_component parent);
   extern virtual function void build_phase(uvm_phase phase);
   extern virtual function void end_of_elaboration_phase(uvm_phase phase);
   extern virtual task run_phase(uvm_phase phase);
 
-endclass : out_monitor
+endclass : out_mon
 
 //--------------------------------------------------------------------------------------------
 // Construct: new
 //  Initializes memory for new object
 //
 // Parameters:
-//  name   - out_monitor
+//  name   - out_mon
 //  parent - parent under which this component is created
 //--------------------------------------------------------------------------------------------
-function out_monitor::new(string name = "out_monitor", uvm_component parent);
+function out_mon::new(string name = "out_mon", uvm_component parent);
   super.new(name, parent);
-  out_monitor_port = new("out_monitor_port", this);
+  out_mon_port = new("out_mon_port", this);
 endfunction : new
 
 //--------------------------------------------------------------------------------------------
@@ -50,7 +50,7 @@ endfunction : new
 // Parameters:
 //  phase - uvm phase
 //--------------------------------------------------------------------------------------------
-function void out_monitor::build_phase(uvm_phase phase);
+function void out_mon::build_phase(uvm_phase phase);
   super.build_phase(phase);
   if (!uvm_config_db#(virtual pkt_intf)::get(this, "", "pkt_vif", pkt_vif)) begin
     `uvm_fatal("NO PKT_VIF", {"virtual interface must be set for: ", get_full_name(), ".PKT_VIF"});
@@ -63,7 +63,7 @@ endfunction : build_phase
 // Parameters:
 //  phase - uvm phase
 //--------------------------------------------------------------------------------------------
-function void out_monitor::end_of_elaboration_phase(uvm_phase phase);
+function void out_mon::end_of_elaboration_phase(uvm_phase phase);
   super.end_of_elaboration_phase(phase);
   // Add any end-of-elaboration configuration here if needed
 endfunction : end_of_elaboration_phase
@@ -73,7 +73,7 @@ endfunction : end_of_elaboration_phase
 // Parameters:
 //  phase - uvm phase
 //--------------------------------------------------------------------------------------------
-task out_monitor::run_phase(uvm_phase phase);
+task out_mon::run_phase(uvm_phase phase);
   super.run_phase(phase);
   
   forever begin
@@ -96,7 +96,7 @@ task out_monitor::run_phase(uvm_phase phase);
                                       in_seq_item_h.pkt_rx_mod,in_seq_item_h.pkt_rx_err), UVM_LOW);
     
     // calling the write function
-    out_monitor_port.write(in_seq_item_h);
+    out_mon_port.write(in_seq_item_h);
   end
 
 endtask : run_phase
