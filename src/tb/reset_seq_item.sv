9fe534aaa8781aecde2da7e9e6db2db25477f342
01sharanshetty <126483491+01sharanshetty@users.noreply.github.com>
2024-07-11 16:26:52 +0530

    Update reset_seq_item.sv

--- src/tb/reset_seq_item.sv
+++ src/tb/reset_seq_item.sv
@@ -3,14 +3,14 @@
 
 class reset_seq_item extends uvm_sequence_item;
 
+//RESET SIGNALS
+rand bit reset_156m25_n;
+rand bit wb_rst_i;
+
 `uvm_object_utils_begin(reset_seq_item)
 `uvm_field_int(reset_156m25_n, UVM_ALL_ON)
 `uvm_field_int(wb_rst_i, UVM_ALL_ON)
 `uvm_object_utils_end
-
-//RESET SIGNALS
-rand bit reset_156m25_n;
-rand bit wb_rst_i;
 
 //Class Constructor
 function new(string name = "reset_seq_item");
