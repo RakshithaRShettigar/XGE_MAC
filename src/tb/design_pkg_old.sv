f0d1fce2a59e669b9cd2708f325459889263dafe
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2024-07-15 14:45:18 +0530

    Update design_pkg_old.sv

--- src/tb/design_pkg_old.sv
+++ src/tb/design_pkg_old.sv
@@ -1,30 +1,30 @@
-// `ifndef DESIGN_PKG_OLD_INCLUDED_
-// `define DESIGN_PKG_OLD_INCLUDED_
+`ifndef DESIGN_PKG_OLD_INCLUDED_
+`define DESIGN_PKG_OLD_INCLUDED_
 
 
 
 
-// `include "../rtl/fault_sm.sv"
-// `include "../rtl/generic_mem_small.sv"
-// `include "../rtl/generic_mem_medium.sv"
-// `include "../rtl/generic_fifo_ctrl.sv"
-// `include "../rtl/generic_fifo.sv"
-// `include "../rtl/meta_sync.sv"
-// `include "../rtl/meta_sync_single.sv"
-// `include "../rtl/rx_hold_fifo.sv"
-// `include "../rtl/rx_data_fifo.sv"
-// `include "../rtl/rx_dequeue.sv"
-// `include "../rtl/rx_enqueue.sv"
-// `include "../rtl/sync_clk_core.sv"
-// `include "../rtl/sync_clk_wb.sv"
-// `include "../rtl/sync_clk_xgmii_tx.sv"
-// `include "../rtl/tx_hold_fifo.sv"
-// `include "../rtl/tx_data_fifo.sv"
-// `include "../rtl/tx_dequeue.sv"
-// `include "../rtl/tx_enqueue.sv"
-// `include "../rtl/wishbone_if.sv"
-// `include "../rtl/xge_mac.sv"
+`include "../rtl/fault_sm.sv"
+`include "../rtl/generic_mem_small.sv"
+`include "../rtl/generic_mem_medium.sv"
+`include "../rtl/generic_fifo_ctrl.sv"
+`include "../rtl/generic_fifo.sv"
+`include "../rtl/meta_sync.sv"
+`include "../rtl/meta_sync_single.sv"
+`include "../rtl/rx_hold_fifo.sv"
+`include "../rtl/rx_data_fifo.sv"
+`include "../rtl/rx_dequeue.sv"
+`include "../rtl/rx_enqueue.sv"
+`include "../rtl/sync_clk_core.sv"
+`include "../rtl/sync_clk_wb.sv"
+`include "../rtl/sync_clk_xgmii_tx.sv"
+`include "../rtl/tx_hold_fifo.sv"
+`include "../rtl/tx_data_fifo.sv"
+`include "../rtl/tx_dequeue.sv"
+`include "../rtl/tx_enqueue.sv"
+`include "../rtl/wishbone_if.sv"
+`include "../rtl/xge_mac.sv"
 
 
 
-// `endif
+`endif
