1528fa7012eb214200acde983c95d0ae1eea276a
Sameeksha P U <109358747+Sameekshapu@users.noreply.github.com>
2024-07-16 23:37:26 -0700

    Update wish_seqr.sv

--- src/tb/wish_seqr.sv
+++ src/tb/wish_seqr.sv
@@ -1,8 +1,21 @@
 `ifndef WISH_SEQR_INCLUDED_
 `define WISH_SEQR_INCLUDED_
+//--------------------------------------------------------------------------------------------
+// Class: wish_seqr
+//--------------------------------------------------------------------------------------------
+
 
 class wish_seqr extends uvm_sequencer#(wish_seq_item);
   `uvm_component_utils(wish_seqr)
+
+//--------------------------------------------------------------------------------------------
+// Construct: new
+//  Initializes the wish sequencer class component
+//
+// Parameters:
+//  name - wish_sequencer
+//  parent - parent under which this component is created
+//--------------------------------------------------------------------------------------------
   
   function new(string name = "wish_seqr", uvm_component parent);
     super.new(name, parent);
